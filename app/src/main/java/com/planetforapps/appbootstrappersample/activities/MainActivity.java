b892d5b0d29c4d52c1a72528531848942ce6392e
Dinuka Jayasuriya <dinukapps@gmail.com>
2018-02-23 16:06:07 +0800

    Initial commit

---
+++ app/src/main/java/com/planetforapps/appbootstrappersample/activities/MainActivity.java
@@ -0,0 +1,15 @@
+package com.planetforapps.appbootstrappersample.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.planetforapps.appbootstrappersample.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
